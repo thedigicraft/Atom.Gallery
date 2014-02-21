d4369a2490c5dc84447d96a2a0d29886cc396199
Alan Quandt <acc@rtistics.com>
2014-02-21 01:16:01 -0500

    Initial commit
    This is a working version of the plugin. However, not a finished
    version.

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Simple Photo Gallery
+
+This is a very simple jQuery photo gallery for use with AtomCMS 2.0.
+
+### Status
+
+This gallery is still being developed.  
---
+++ src/js/jquery.atomGallery.js
@@ -0,0 +1,20 @@
+// Atom Gallery 1.0
+// Developed by: Alan Quandt
+// Group: The Digital Craft
+
+
+$.fn.atomGallery = function() {
+
+	$(".thumbnail").on('click', function() {
+		
+		event.preventDefault();
+		
+		var imageloaded = $(this).attr('href');
+				
+		$(".atom-gallery #holder img").replaceWith('<img class="img-thumbnail" src="'+imageloaded+'">');
+	
+	});
+
+
+
+};
