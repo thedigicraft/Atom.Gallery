28cc25587426276e9928fe716ed49a662472ad06
Alan Quandt <acc@rtistics.com>
2014-02-22 23:35:38 -0500

    Fixed hover bug
    Thumbnail CSS behaves properly now.

--- src/js/jquery.atomGallery.js
+++ src/js/jquery.atomGallery.js
@@ -1,23 +1,30 @@
 // Atom Gallery 1.0
 // Developed by: Alan Quandt
 // Group: The Digital Craft
+// WWW: TheDigitalCraft.com
 
 
 $.fn.atomGallery = function() {
-
+	
+	$(".thumbnails img").toggleClass("inactive", true); // Reset all images to slightly faded.
+	
+	// Handle what happens when a thumbnail is clicked:
 	$(".thumbnail").on('click', function() {
 		
-		event.preventDefault();
+		event.preventDefault(); // We don't want the links to refresh the page.
 		
-		var imageloaded = $(this).attr('href');
+		var imageloaded = $(this).attr('href'); // Store the URL of the image that has been clicked.
 				
-		$(".atom-gallery #holder img").replaceWith('<img class="img-thumbnail" src="'+imageloaded+'">');
+		$(".atom-gallery #holder img").replaceWith('<img class="img-thumbnail" src="'+imageloaded+'">'); // Change out the current image.
+
+		// Handle the style changes:
+			
+			// Reset the style:
+			$(".thumbnails img").removeClass("active"); // Clear 'active' class from all images.
+			$(".thumbnails img").addClass("inactive"); // Reset all images to be inactive (faded). 
 		
-		$(".thumbnails img").css("opacity", ".8");
-		$("> img", this).css("opacity", "1");
+		$("> img", this).addClass("active"); // Set selected image to be active (bright).
 	
-	});
-
-
-
+	}); // END .thumbnail on click
+		
 };
